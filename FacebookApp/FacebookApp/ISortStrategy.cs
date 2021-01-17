59c89f5c035db7dcbcafbf532d0e7a4c5beacfc0
Omri Zuta <omrizu@mta.ac.il>
2021-01-17 12:49:00 +0200

    started strategy pattern

---
+++ FacebookApp/FacebookApp/ISortStrategy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FacebookApp
+{
+    interface ISortStrategy
+    {
+        List<T> SortList<T>(List<T> i_List);
+    }
+}
